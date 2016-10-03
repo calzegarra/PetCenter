c1cafed49b92faf4c7ab1cade5693990ed2c60d0
Willy Aguirre <marti1125@gmail.com>
2016-10-03 18:48:44 -0500

    se agrega las vistas: index y clientesmascotas

--- src/main/java/com/petcenter/controller/Index.java
+++ src/main/java/com/petcenter/controller/Index.java
@@ -3,22 +3,20 @@ package com.petcenter.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Index {
 	
 	@RequestMapping("/index")
 	public String index(Model model) {
-		String val = "Willy";
-		model.addAttribute("name", val);
+		model.addAttribute("titulo", "Pet Center");
 		return "index";
 	}
 	
-	@RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false) String name, Model model) { //defaultValue="World"
-        model.addAttribute("name", "fff");
-        return "greeting";
-    }
+	@RequestMapping("/clientesmascotas")
+	public String clientesmascotas(Model model) {
+		model.addAttribute("titulo", "Pet Center");
+		return "clientesmascotas";
+	}
 	
 }
